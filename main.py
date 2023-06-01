f7ffaa0d005466d5123c39573047253cd6613eaa
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-06-01 20:14:38 +0300

    ex 1/11

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+class BankAccount:
+    def __init__(self, account_number, balance):
+        self.account_number = account_number
+        self.balance = balance
 
-    def get_info(self):
-        print("Ім'я студента:", self.name)
-        print("Вік студента:", self.age)
-student1 = Student("HolyMoly", 20)
-student1.get_info()
+    def deposit(self, amount):
+        self.balance += amount
+        print("Гроші успішно додані на рахунок. Поточний баланс:", self.balance)
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            print("Гроші успішно зняті з рахунку. Поточний баланс:", self.balance)
+        else:
+            print("Недостатньо коштів на рахунку.")
+
+    def get_balance(self):
+        print("Поточний баланс:", self.balance)
+        account1 = BankAccount("123456789", 1000)
+        account1.get_balance()
+        account1.deposit(500)
+        account1.withdraw(200)
+        account1.get_balance()
