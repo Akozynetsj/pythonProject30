d045781195765f6ef39ac396fadb5f40d7ec1af4
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-06-01 19:41:21 +0300

    ex 1/6

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def is_palindrome(word):
-    word = word.replace(" ", "").lower()
-    if word == word[::-1]:
-        return True
-    else:
-        return False
-        input_word = input("Введіть слово: ")
-        if is_palindrome(input_word)
-            print("Це слово є паліндромом")
-            else:
-            print("Це слово не є паліндромом")
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_numbers = [num for num in numbers if num % 2 == 0]
+print("Список парних чисел:", even_numbers)
+
