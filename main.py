e0ccc65a359c7f45b976379bb6e7e9038d3ce593
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-06-01 19:35:26 +0300

    ex 1/2

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Привіт, світ!")
+number1 = int(input("Введіть перше число: "))
+number2 = int(input("Введіть друге число: "))
+start = min(number1, number2)
+end = max(number1, number2)
+sum_of_range = 0
+for num in range(start+1, end):
+    sum_of_range += num
+print("Сума чисел у діапазоні між", start, "і", end, "дорівнює", sum_of_range)
