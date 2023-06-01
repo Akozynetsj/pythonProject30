5139fa8a1ee109414dc63cc9bca5977cd31418e9
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-06-01 19:40:08 +0300

    ex 1/5

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-input_string = input("Введіть рядок: ")
-for char in input_string:
-    print(char)
-
+def is_palindrome(word):
+    word = word.replace(" ", "").lower()
+    if word == word[::-1]:
+        return True
+    else:
+        return False
+        input_word = input("Введіть слово: ")
+        if is_palindrome(input_word)
+            print("Це слово є паліндромом")
+            else:
+            print("Це слово не є паліндромом")
