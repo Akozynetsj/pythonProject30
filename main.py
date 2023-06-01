6095be69507103e020d5ab25dc41e84355e8cfc5
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-06-01 19:38:06 +0300

    ex 1/4

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sum_of_even = 0
-for num in range(1, 101):
-    if num % 2 == 0:
-        sum_of_even += num
-print("Сума парних чисел від 1 до 100 дорівнює", sum_of_even)
+input_string = input("Введіть рядок: ")
+for char in input_string:
+    print(char)
+
