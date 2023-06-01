b7a3e71c8e7d306c39da835f257eb40205d8d2bc
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-06-01 19:47:51 +0300

    ex 1/9

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def filter_capitalized_strings(string_list):
+def filter_python_strings(string_list):
     filtered_list = []
     for string in string_list:
-        if string[0].isupper():
+        if "Python" in string:
             filtered_list.append(string)
             return filtered_list
             input_list = input("Введіть список рядків (розділених пробілами): ").split()
-            filtered_strings = filter_capitalized_strings(input_list)
+            filtered_strings = filter_python_strings(input_list)
             print("Результат:", filtered_strings)
 
