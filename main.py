0d6ea017a9c929e7414c6d60eeedc863fce0c165
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-06-01 20:21:03 +0300

    ex 1/12

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-class BankAccount:
-    def __init__(self, account_number, balance):
-        self.account_number = account_number
-        self.balance = balance
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
 
-    def deposit(self, amount):
-        self.balance += amount
-        print("Гроші успішно додані на рахунок. Поточний баланс:", self.balance)
-
-    def withdraw(self, amount):
-        if self.balance >= amount:
-            self.balance -= amount
-            print("Гроші успішно зняті з рахунку. Поточний баланс:", self.balance)
-        else:
-            print("Недостатньо коштів на рахунку.")
-
-    def get_balance(self):
-        print("Поточний баланс:", self.balance)
-        account1 = BankAccount("123456789", 1000)
-        account1.get_balance()
-        account1.deposit(500)
-        account1.withdraw(200)
-        account1.get_balance()
+    def get_info(self):
+        print("Назва книги:", self.title)
+        print("Автор:", self.author)
+book1 = Book("Майстер і Маргарита", "Михайло Булгаков")
+book1.get_info()
